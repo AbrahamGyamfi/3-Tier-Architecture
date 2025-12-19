d13bcf9013facc8dd34e37755589f3ac9d7b1753
AbrahamGyamfi <gyamfiabraham95@email.com>
2025-12-19 08:33:11 +0000

    .

--- provider.tf
+++ provider.tf
@@ -1,9 +1,16 @@
 # Specify Terraform Version
 terraform {
+  required_version = ">= 1.0"
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "6.25.0"
     }
   }
+}
+
+# Configure AWS Provider
+provider "aws" {
+  region = var.aws_region
 }
